844cf38d39dc68f655c80251ae8ee1a72a6476da
BIMLESH5560 <128776228+BIMLESH5560@users.noreply.github.com>
2024-09-24 07:26:42 +0000

    container config done

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+const accountId=5387367;
+let accountEmail="bimlesh102030@gmail.com";
+var accountPassword="53633";
+accountCity="Bihar";
+let accountState;
+console.log(accountCity);
+/*
+
+*/
+
+//accountId=2;// it is not allowed
+accountEmail="bimlesh5366@gmail.com";
+accountPassword="646746";
+accountCity="Ghaziabad";
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState]);
+
+
