037255e356a1e6ee1f3021c000dfabd44bc3cdf3
BIMLESH5560 <128776228+BIMLESH5560@users.noreply.github.com>
2024-09-26 13:59:54 +0000

    container config done

---
+++ 01_basic/02_DataType.js
@@ -0,0 +1,20 @@
+//"use strict", treat all js code as newer version
+//alert(3+4); we are using node.js not a broser
+let name="bimlesh kumar";
+let age=23;
+let isLogin=true;
+
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol=> unique
+
+//Object
+
+console.log(typeof null);
+
+
+
