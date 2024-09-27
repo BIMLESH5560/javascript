77ded13d25aef007d5dd5269aa8a086f6579e6b0
BIMLESH5560 <128776228+BIMLESH5560@users.noreply.github.com>
2024-09-27 14:56:59 +0000

    conversions of DataType

---
+++ 01_basic/03_conversions.js
@@ -0,0 +1,26 @@
+let score="66abc";
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let value=Number(score);
+//console.log(typeof value);
+//console.log(value);
+
+//"66"=>66
+//"66gdg"=>NaN
+//true =>1; false =>0;
+
+let isLoggedIn =1;
+let booleanisLoggedIn = Boolean (isLoggedIn);
+console.log(booleanisLoggedIn);
+
+//1 => true; 0=> false
+// " " => false
+// "bimlesh" => true
+
+let someNumber=66;
+let stringNumber= String(someNumber);
+console.log( typeof stringNumber);
+
+
+
