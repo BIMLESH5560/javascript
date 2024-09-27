97352e7b713c8989059f4eb7d5b127ba757a3b22
BIMLESH5560 <128776228+BIMLESH5560@users.noreply.github.com>
2024-09-27 15:49:30 +0000

    conversions of DataType

--- 01_basic/03_conversions.js
+++ 01_basic/03_conversions.js
@@ -19,8 +19,56 @@ console.log(booleanisLoggedIn);
 // "bimlesh" => true
 
 let someNumber=66;
-let stringNumber= String(someNumber);
-console.log( typeof stringNumber);
+//let stringNumber= String(someNumber);
+//console.log( typeof stringNumber);
+
+let value=77;
+let negValue=-value;
+//console.log(negValue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2%2);
+console.log(2*2);
+console.log(2**2);
+*/
+
+let str1="hello";
+let str2=" bimlesh";
+let str3= str1 + str2;
+console.log(str3);
+
+// console.log("2" + 2 );
+// console.log(2 + "2");
+// console.log("2" + 2 + 2);
+// console.log( 2 + 2 + "2");
+
+console.log(+true);
+console.log(+false);
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+// link to study-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
